b158f67a360080abac5cefb302af7f50899f0370
praveenprk <praveenkumarkp666@gmail.com>
2022-03-05 00:53:50 +0530

    wrapping app with provider

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Auth from './features/Auth/Auth';
 
 function App() {
   return (
+    //Here, you're saying that, Provide
+    //me the store that I created
+    //hence <Provider store={store}>
+    
     <Provider store={store}>
       <div className="App">
         {/* <Counter/> */}
